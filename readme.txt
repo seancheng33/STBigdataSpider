319f48e959403be25a14f1144f32dd27a4686644
seancheng33 <seancheng33@gmail.com>
2017-11-03 20:58:21 +0800

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,48 @@
+﻿开发运行环境：
+python-3.6.2
+
+所需用到的插件：
+beautifulsoup4==4.6.0
+bs4==0.0.1
+configparser==3.5.0
+lxml==4.1.0
+PySocks==1.6.7
+selenium==3.6.0
+
+可用使用下面的命令批量安装模块：
+pip install -r requests.txt
+
+
+config.ini配置说明，请按实际部署的情况修改config.ini中的以下内容
+
+[spider]#爬取数据功能的配置
+url = #需要打开的网址，例：http://10.245.254.139:7180/cmf/home
+username = #登录的用户名，例：admin
+password = #登录的密码，例：admin
+checkstatus = #需要检查的状态的定义字段，具体见下面的列表，建议将下面的''内的内容直接复制过去 例：cm-icon-status-bad-health
+[proxy] #邮件功能的网络代理的配置
+url = #代理服务器的地址，例：cmproxy.gmcc.net 或 127.0.0.1 可以是域名或IP
+port = #代理服务器的端口，例：8081 或 8080
+
+[mail] #邮件发送的配置
+host = #邮件smtp服务器，测试了139邮箱，可以发送，例：smtp.139.com
+name = #登陆用户名，例：username
+password = #登陆密码，例：password
+sender = #发送者邮箱，例：xxx@139.com
+to_receivers = #收件的邮箱地址，可以多个也可以一个，多个邮箱用英文的逗号“,”分隔，例： xxx@163.com,xxx@qq.com，xxx@139.com
+cc_receivers = #抄送的邮箱地址，可以多个也可以一个或者留空，多个邮箱用英文的逗号“,”分隔，例： xxx@163.com,xxx@qq.com，xxx@139.com
+
+
+需要检查状态的定义字段，此内容根据cdh的页面的css分析得出
+'cm-icon-status-unknown': '未知',页面的显示图标为
+'cm-icon-status-history-not-available': '（未知）',
+'cm-icon-status-down': '（未知）',
+'cm-icon-status-stopping': '正在停止',
+'cm-icon-status-starting': '正在启动',
+'cm-icon-status-disabled-health': '已禁用的运行状况',
+'cm-icon-status-stopped': '已停止',
+'cm-icon-status-none': '无'     在页面的显示图标为灰色
+'cm-icon-status-unknown-health': '未知运行状况',
+'cm-icon-status-bad-health': '运行状况不良'     在页面的显示图标为红色  （本应用脚本主要就是要抓取这个状态的信息）
+'cm-icon-status-good-health': '运行状态良好'    在页面的显示图标为绿色
+'cm-icon-status-concerning-health': '存在隐患的运行状况'     页面的显示图标为黄色
---
+++ sendmail.py
@@ -0,0 +1,57 @@
+'''测试发送邮件功能  '''
+import smtplib, socks, logging,time
+from email.header import Header
+from email.mime.text import MIMEText
+
+
+
+class SendMail():
+    def __init__(self, mail_host, mail_user, mail_pass, sender, to_receivers, cc_receivers,mail_content):
+        logging.basicConfig(filename=time.strftime('%Y%m%d', time.localtime(time.time())) + '.log', level=logging.DEBUG)
+        self.mail_host = mail_host
+        self.mail_user = mail_user
+        self.mail_pass = mail_pass
+        self.sender = sender
+        self.to_receivers = to_receivers
+        self.cc_receivers = cc_receivers
+
+        # 邮件正文内容
+        self.mail_content = '<p>以下为运行状态良好的各主机状态</p>' + mail_content + \
+                    '<div style="margin: 10.0px;position: static;">' \
+                    '<span style="color: rgb(0,0,0);font-family: Verdana , 微软雅黑 , 宋体 , sans-serif;font-size: 10.0pt;">祝安康<br></span>' \
+                    '<span style="color: rgb(0,0,0);font-family: Verdana , 微软雅黑 , 宋体 , sans-serif;font-size: 10.0pt;">业务支持中心-信息管理室</span>' \
+                    '<span style="color: rgb(0,0,0);font-family: Verdana , 微软雅黑 , 宋体 , sans-serif;font-size: 10.0pt;"><br>' \
+                    '<br>IT工作台：支撑单记录小组--》<br>' \
+                    '邮箱地址：<a href="mailto:gyyzzxyw@gd.chinamobile.com" target="_blank">gyyzzxyw@gd.chinamobile.com</a><br>' \
+                    '服务热线：13929648834转1<br>' \
+                    '以上内容由python脚本自动采集并发送。</div></div></div>'
+        # 三个参数：
+        # 第一个为文本内容
+        # 第二个 plain 设置文本格式，如果需要HTML格式，修改成 html 即可，正文中的内容需要写成html代码的格式
+        # 第三个 utf-8 设置编码格式
+        # message = MIMEText(text, 'plain', 'utf-8')
+        self.message = MIMEText(self.mail_content, 'html', 'utf-8')
+
+        self.message['From'] = Header(self.sender, 'utf-8')  # 这一项显示为发件人的内容
+        # message['To'] = Header(str(receivers), 'utf-8')  # 这一项显示为收件人的内容
+        self.message['To'] = ','.join(self.to_receivers)
+        # ccaddr =['','']抄送名单
+        self.message['Cc'] = ','.join(self.cc_receivers)  # 这一项显示为抄送收件人的内容，下面的发送记得将抄送名单也加进去
+        subject = '测试python邮件发送功能'
+        self.message['Subject'] = Header(subject, 'utf-8')
+
+    def send(self,proxy_url, proxy_port):
+        # 需要使用代理访问网络才可以发送邮件。
+        socks.setdefaultproxy(socks.PROXY_TYPE_HTTP, proxy_url, proxy_port)
+        socks.wrapmodule(smtplib)
+        # 发送邮件，捕捉异常
+        try:
+            smtpObj = smtplib.SMTP(self.mail_host, 25)
+            # 登录邮箱
+            smtpObj.login(self.mail_user, self.mail_pass)
+            smtpObj.sendmail(self.sender, self.to_receivers + self.cc_receivers, self.message.as_string())
+            smtpObj.quit()  # 关闭连接
+            logging.info("邮件发送成功")
+        except smtplib.SMTPException:
+            logging.info("Error: 无法发送邮件")
+
